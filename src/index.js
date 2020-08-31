eb4a1fa215e2cf6ca0eb311a444398b2c1c6936c
Younes <47226184+Younes-Asl@users.noreply.github.com>
2020-08-31 20:38:59 +0200

    Add command variants

--- src/index.js
+++ src/index.js
@@ -17,6 +17,57 @@ const board = new five.Board();
 
 let strip;
 
+/**
+ * Returns a pixel index from the given arguments.
+ * Returns null if the index is not valid (e.g. < 0 or > 39).
+ * @param {string[]} args
+ */
+const getPixelIndex = (args) => {
+  const input = args.join(' ');
+  if (input.match(/^r /i)) return Math.floor(Math.random() * 40);
+  const fromIndex = input.match(/^index (\d+)/i);
+  const fromCoords = input.match(/^x (\d+) y (\d+)/i);
+  const fromRowCol = input.match(/^row (\d+) col (\d+)/i);
+  if (fromIndex) {
+    const index = parseInt(fromIndex[1]);
+    if (index < 0 || index > 39) return null;
+    return index;
+  }
+  if (fromCoords) {
+    const x = parseInt(fromCoords[1]);
+    const y = parseInt(fromCoords[2]);
+    if (x < 0 || x > 7) return null;
+    if (y < 0 || y > 4) return null;
+    return y * 8 + x;
+  }
+  if (fromRowCol) {
+    const row = parseInt(fromCoords[1]);
+    const col = parseInt(fromCoords[2]);
+    if (col < 0 || col > 7) return null;
+    if (row < 0 || row > 4) return null;
+    return row * 8 + col;
+  }
+  return null;
+};
+
+/**
+ * Returns a color from the given arguments.
+ * Returns null if the color is not valid.
+ * @param {string[]} args 
+ */
+const getColor = (args) => {
+  const input = args.join(' ');
+  if (input.match(/^r /i)) {
+    return ['r', 'g', 'b'].map(() => {
+      const hex = Math.floor(Math.random() * 256).toString(16);
+      return hex.length === 1 ? `0${hex}` : hex;
+    }).join();
+  }
+  const matches = input.match(/color ([0-9a-f]{6})$/i);
+  if (matches) return matches[1];
+  return null;
+};
+
 const validColor = (color) => color.match(/^[0-9a-f]{6}$/i);
 
 const lightenColor = (color) => {
@@ -49,17 +100,12 @@ board.on('ready', () => {
       if (message.startsWith('!')) {
         const args = message.split(' ');
         const command = args.shift();
-        // TODO: !pixel index 0 color 00FF00
-        // TODO: !pixel x 3 y 4 color 00FF00
-        // TODO: !pixel row 3 col 4 color 00FF00
-        // TODO: !pixel r
         if (command === '!pixel') {
           if (!args.length) return;
-          const pixelIndex = parseInt(args.shift(), 10);
-          if (isNaN(pixelIndex)) return;
-          if (pixelIndex < 0 || pixelIndex > 39) return;
-          const color = args.shift();
-          if (!color || !validColor(color)) return;
+          const pixelIndex = getPixelIndex(args);
+          if (!pixelIndex) return;
+          const color = getColor(args);
+          if (!color) return;
           const lightenedColor = lightenColor(color);
           strip.pixel(pixelIndex).color(lightenedColor);
           strip.show();
